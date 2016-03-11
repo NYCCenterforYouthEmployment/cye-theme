c2da2c2d34bb20872dbfe184327d316a54541d39
Cyril Tsiboulski <ct@cloudred.com>
2016-03-11 15:44:56 -0500

    added logic to print all partners on pages outside of home

--- templates/content-partners.php
+++ templates/content-partners.php
@@ -9,31 +9,41 @@
 				$partner_link = $partner['partner_link'];
 				$partner_logo = $partner['partner_logo'];
 				//print if logo exists and this is a founding partner
-				if( !empty($partner_logo) && $partner_status ):
-                	$url = $partner_logo['url'];
-                    $title = $partner_logo['title'];
-                    $alt = $partner_logo['alt'];
-                    $caption = $partner_logo['caption'];
-                           
-                    // thumbnail
-                    $size = 'thumbnail';
-                    $thumb = $partner_logo['sizes'][ $size ];
-                    $width = $partner_logo['sizes'][ $size . '-width' ];
-                    $height = $partner_logo['sizes'][ $size . '-height' ];
-                    //print logo
-					?>
-                    <li>
-    					<?php if (!empty($partner_link)): ?>
-    						<a href="<?php echo $partner_link; ?>" target="_blank">
-    					<?php endif; ?>
-    					<img src="<?php echo $thumb; ?>" alt="<?php echo $alt; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" />
-    					<?php if (!empty($partner_link)): ?>
-    						</a>
-    					<?php endif; ?>
-  					</li>
-  				<?php
-                   endif;
-				}
+				if( !empty($partner_logo) ):
+                	if ( is_page_template( 'home.php' ) ):
+						if ( $partner_status ):
+							print_partner();
+						endif;
+					else :
+						print_partner();
+					endif;
+					
+                endif;
 			}
-        ?>
+		}
+		
+		function print_partner() {
+			$url = $partner_logo['url'];
+            $title = $partner_logo['title'];
+            $alt = $partner_logo['alt'];
+			$caption = $partner_logo['caption'];
+                           
+            // thumbnail
+            $size = 'thumbnail';
+            $thumb = $partner_logo['sizes'][ $size ];
+            $width = $partner_logo['sizes'][ $size . '-width' ];
+            $height = $partner_logo['sizes'][ $size . '-height' ];
+            //print logo
+			?>
+            <li>
+    		<?php if (!empty($partner_link)): ?>
+    				<a href="<?php echo $partner_link; ?>" target="_blank">
+    		<?php endif; ?>
+    		<img src="<?php echo $thumb; ?>" alt="<?php echo $alt; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" />
+    		<?php if (!empty($partner_link)): ?>
+    		</a>
+    		<?php endif; ?>
+        <?php } ?>
+  			</li>
+ 
 </ul>
