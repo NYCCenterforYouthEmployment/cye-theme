18b3b483ec9ee959a46f0a074cba977876cfc36c
Cyril Tsiboulski <ct@cloudred.com>
2016-03-11 16:27:04 -0500

    reverting

--- templates/content-partners.php
+++ templates/content-partners.php
@@ -4,47 +4,36 @@
        if($partners){
        		shuffle( $partners );
        		foreach($partners as $partner) {
+            	$partner_name = $partner['partner_name'];
+				$partner_status = $partner['partner_status'];
+				$partner_link = $partner['partner_link'];
+				$partner_logo = $partner['partner_logo'];
 				//print if logo exists and this is a founding partner
-				if( !empty($partner_logo) ):
-                	if ( is_page_template( 'home.php' ) ):
-						if ( $partner_status ):
-							print_partner($partner);
-						endif;
-					else :
-						print_partner($partner);
-					endif;
-					
-                endif;
-			}
-		}
-		
-		function print_partner($partner) {
-			$partner_name = $partner['partner_name'];
-			$partner_status = $partner['partner_status'];
-			$partner_link = $partner['partner_link'];
-			$partner_logo = $partner['partner_logo'];
-				
-			$url = $partner_logo['url'];
-            $title = $partner_logo['title'];
-            $alt = $partner_logo['alt'];
-			$caption = $partner_logo['caption'];
+				if( !empty($partner_logo) && $partner_status ):
+                	$url = $partner_logo['url'];
+                    $title = $partner_logo['title'];
+                    $alt = $partner_logo['alt'];
+                    $caption = $partner_logo['caption'];
                            
-            // thumbnail
-            $size = 'thumbnail';
-            $thumb = $partner_logo['sizes'][ $size ];
-            $width = $partner_logo['sizes'][ $size . '-width' ];
-            $height = $partner_logo['sizes'][ $size . '-height' ];
-            //print logo
-			?>
-            <li>
-    		<?php if (!empty($partner_link)): ?>
-    				<a href="<?php echo $partner_link; ?>" target="_blank">
-    		<?php endif; ?>
-    		<img src="<?php echo $thumb; ?>" alt="<?php echo $alt; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" />
-    		<?php if (!empty($partner_link)): ?>
-    		</a>
-    		<?php endif; ?>
-        <?php } ?>
-  			</li>
- 
+                    // thumbnail
+                    $size = 'thumbnail';
+                    $thumb = $partner_logo['sizes'][ $size ];
+                    $width = $partner_logo['sizes'][ $size . '-width' ];
+                    $height = $partner_logo['sizes'][ $size . '-height' ];
+                    //print logo
+					?>
+                    <li>
+    					<?php if (!empty($partner_link)): ?>
+    						<a href="<?php echo $partner_link; ?>" target="_blank">
+    					<?php endif; ?>
+    					<img src="<?php echo $thumb; ?>" alt="<?php echo $alt; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" />
+    					<?php if (!empty($partner_link)): ?>
+    						</a>
+    					<?php endif; ?>
+  					</li>
+  				<?php
+                   endif;
+				}
+			}
+        ?>
 </ul>
