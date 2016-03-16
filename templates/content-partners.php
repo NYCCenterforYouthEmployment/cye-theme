bf199d54928584a41e66cca119eb7ac4ff575ebf
Al Sierra <al@cloudred.com>
2016-03-16 19:28:42 -0400

    Update header

--- templates/content-partners.php
+++ templates/content-partners.php
@@ -23,7 +23,7 @@
 	?>
 </ul>
 <?php if($non_founding_partners && is_page_template( 'partners.php' )): ?>
-	<h2>Corporate Sponsors</h2>
+	<h2>Key Funder and Employer Partners</h2>
 	<ul class="partner-listing">
 	  <?php
 		foreach($non_founding_partners as $partner) {
