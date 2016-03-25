f53c67df72fb0eb8f4d78c17853f6ab1b3835e1d
Cyril Tsiboulski <ct@cloudred.com>
2016-03-25 13:53:57 -0400

    added city agency partner listing option

--- templates/content-partners.php
+++ templates/content-partners.php
@@ -1,13 +1,15 @@
 <?php if( is_page_template( 'partners.php' )): ?>
-	<h2>Founding Partners</h2>
+	<h2><?php echo __('Founding Partners', 'sage'); ?></h2>
 <?php endif; ?>
 <ul class="partner-listing">
   <?php
        $partners = get_field('partner',$partners_page_id);
        $non_founding_partners = array();
+	   $city_agency_partners = array();
        if($partners){
        		foreach($partners as $partner) {
             	$partner_name = $partner['partner_name'];
+				$partner_type = $partner['partner_type'];
 				$partner_status = $partner['partner_status'];
 				$partner_link = $partner['partner_link'];
 				$partner_logo = $partner['partner_logo'];
@@ -15,15 +17,18 @@
 				if( !empty($partner_logo) && !empty($partner_status)){
 					include(locate_template('templates/partner-logo-list-item.php'));
                 }
-                if(empty($partner_status)){
+                if(empty($partner_status) && $partner_type == 'key-funder'){
 					$non_founding_partners [] = $partner;
                 }
+				if($partner_type == 'city-agency'){
+					$city_agency_partners [] = $partner;
+				}
 			}
 		}
 	?>
 </ul>
 <?php if($non_founding_partners && is_page_template( 'partners.php' )): ?>
-	<h2>Key Funder and Employer Partners</h2>
+	<h2><?php echo __('Key Funder and Employer Partners', 'sage'); ?></h2>
 	<ul class="partner-listing">
 	  <?php
 		foreach($non_founding_partners as $partner) {
@@ -31,7 +36,25 @@
 			$partner_status = $partner['partner_status'];
 			$partner_link = $partner['partner_link'];
 			$partner_logo = $partner['partner_logo'];
-			//print if logo exists and is not a founding partner
+			//print if logo exists
+			if( !empty($partner_logo) ){
+				include(locate_template('templates/partner-logo-list-item.php'));
+			}
+		}
+		?>
+	</ul>
+<?php endif; ?>
+
+<?php if($city_agency_partners && is_page_template( 'partners.php' )): ?>
+	<h2><?php echo __('City Agency Partners', 'sage'); ?></h2>
+	<ul class="partner-listing">
+	  <?php
+		foreach($city_agency_partners as $partner) {
+			$partner_name = $partner['partner_name'];
+			$partner_status = $partner['partner_status'];
+			$partner_link = $partner['partner_link'];
+			$partner_logo = $partner['partner_logo'];
+			//print if logo exists
 			if( !empty($partner_logo) ){
 				include(locate_template('templates/partner-logo-list-item.php'));
 			}
